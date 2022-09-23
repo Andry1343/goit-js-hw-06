0076cdc0e02f242e855d76556df479b6842dfad4
Andry1343 <ua.gavrilets@gmail.com>
2022-09-23 22:00:30 +0300

    Update 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,19 @@ const lengthInput = Number(validInput.dataset.length);
 
 validInput.addEventListener("blur", onBlur);
 
+function onBlur() { 
+  validInput.classList.remove("invalid", "valid"); 
+  const classRt = validInput.value.length === lengthInput ? "valid" : "invalid"
+  validInput.classList.add(classRt); 
+  };
+
+
+
+/* const validInput = document.querySelector("#validation-input");
+const lengthInput = Number(validInput.dataset.length);
+
+validInput.addEventListener("blur", onBlur);
+
 function onBlur() {
   if (validInput.value.length === lengthInput) {
     validInput.classList.remove("invalid");
@@ -14,4 +27,4 @@ function onBlur() {
   if (validInput.value.length === 0) {
     validInput.classList.remove("invalid");
   }
-}; 
+};  */
